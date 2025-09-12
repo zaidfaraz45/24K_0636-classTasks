aae1fc4ae9d31f611dd368f2d6302fc052845a73
zaidfaraz45 <zaidfarazsyed@gmail.com>
2025-09-12 21:28:45 +0500

    Task done

--- lab 3/Task1.cpp
+++ lab 3/Task1.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 struct Node
 {
-    int data;
+    string data;
     Node *next;
 
-    Node(int val)
+    Node(string val)
     {
         data = val;
         next = nullptr;
@@ -25,11 +25,65 @@ class LinkedList
             tail = NULL;
         }
 
-        void insertAtHead(int data)
+        void insertAtHead(string val)
         {
-            Node *temp = new Node(data);
+            Node *temp = new Node(val);
             temp->next = head;
             head = temp;       
+
+            if (tail == NULL)
+            {
+                tail = head;
+            }
+        }
+
+        void insertAtTail(string val)
+        {
+            Node *temp = new Node(val);
+
+            if (head == NULL) 
+            {
+                head = temp;
+                tail = temp;
+            }
+
+            else
+            {
+                tail->next = temp; 
+                tail = temp;       
+            }
+        }
+
+        void insertAfter(string val, string valIns)
+        {
+            Node *prev = head;
+            Node *temp = new Node(valIns);
+
+            while (prev->data != val)
+            {
+                prev = prev->next;
+            }
+
+            temp->next = prev->next->next;
+            prev->next = temp;
+        }
+
+        void search(string val)
+        {
+            Node *temp = head;
+
+            while (temp != tail)
+            {
+                if (temp->data == val)
+                {
+                    cout << "Book found!\n";
+                    return;
+                }
+
+                temp = temp->next;
+            }
+
+            cout << "Book not found!\n";
         }
 
         void display()
@@ -49,8 +103,17 @@ class LinkedList
 int main()
 {
     LinkedList l1;
-    l1.insertAtHead(2);
-    l1.insertAtHead(4);
+    l1.insertAtHead("Data Structures");
+    l1.insertAtTail("Operating Systems");
+    l1.insertAtTail("Data Structures");
+    l1.insertAtTail("Computer Networks");
+    l1.insertAtTail("DataBase Systems");
+
+    l1.insertAtHead("Artificial Intelligence");
+    l1.insertAtTail("Machine Learning");
+    l1.insertAfter("Operating Systems", "Cyber Security");
+
+    l1.search("DataBase Systems");
 
     l1.display();
 
