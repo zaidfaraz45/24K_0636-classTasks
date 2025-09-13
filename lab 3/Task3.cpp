50719775ccbe40e5ab972cfd269b38006a5e5efa
Zaid Faraz <zaidfarazsyed@gmail.com>
2025-09-14 00:14:59 +0500

    Task done

--- lab 3/Task3.cpp
+++ lab 3/Task3.cpp
@@ -1,9 +1,112 @@
 #include <iostream>
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class LinkedList
+{
+    Node *head;
+    Node *tail;
+
+    public:
+        LinkedList()
+        {
+            head = NULL;
+            tail = NULL;
+        }
+
+        void insertAtTail(int comp)
+        {
+            Node *temp = new Node(comp);
+
+            if (head == NULL) 
+            {
+                head = temp;
+                tail = temp;
+            }
+
+            else
+            {
+                tail->next = temp; 
+                temp->prev = tail;
+                tail = temp;       
+            }
+        }
+
+        void deleteHead()
+        {
+            if (head == nullptr) {return;}
+
+            Node* toDelete = head;
+            head = head->next;   
+
+            if (head != nullptr)
+            {
+                head->prev = nullptr; 
+            }
+            else
+            {
+                tail = nullptr;
+            }
+
+            delete toDelete;
+        }
+
+
+        void searchCompartment(int comp)
+        {
+            Node *temp = head;
+
+            while (temp != NULL)
+            {
+                if (temp->data == comp)
+                {
+                    cout << "Comparment number " << comp << " found!\n";
+                    return;
+                }
+
+                temp = temp->next;
+            }
+
+            cout << " Compartment number " << comp << " not in the train!" << endl;
+        }
+
+        void display()
+        {
+            Node *temp = head;
+
+            while(temp != NULL)
+            {
+                cout << temp->data << "->";
+                temp = temp->next;
+            }
+
+            cout << "NULL\n";
+        }
+};
+
 int main()
 {
+    LinkedList train;
     
+    train.insertAtTail(34);
+    train.insertAtTail(46);
+    train.insertAtTail(54);
+
+    train.deleteHead();
+    train.searchCompartment(54);
 
     return 0;
 }
